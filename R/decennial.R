1614fc1d9dac5f6d2afdaa7a4700557ae94e77ed
Christopher Kenny <ctk44@cornell.edu>
2023-08-04 23:14:47 -0400

    Update decennial.R

--- R/decennial.R
+++ R/decennial.R
@@ -1,3 +1,5 @@
-tc_decennial <- function() {
+tc_decennial <- function(geography, variables, year = 2020, state = NULL, county = NULL) {
 
+  req <- httr2::request('https://api.census.gov/data') |>
+    httr2::req_url_path_append(year)
 }
